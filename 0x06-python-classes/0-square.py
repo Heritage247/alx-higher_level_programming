57ba7e8f8be4d8df7acfbbf32ade333dc8a1c279
Heritage247 <dairotolase@gmail.com>
2023-01-24 06:35:54 -0500

    0-sqaure.py corrected

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
+"""Define a class Square."""
+class Square:
+    """Represent a square."""
